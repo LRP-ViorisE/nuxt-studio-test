ff8518d1e4da6117a0bb4d21d8f86c6c5cae6d96
LifeRP - FiveM <contact@liferp.gg>
2023-06-26 11:20:11 +0200

    Updated base info
    Removed excess informations

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    twitter: 'LifeRP-FiveM',
+    github: 'liferp-fivem/nuxt-studio-test'
   },
   prose: {
     h1: {
